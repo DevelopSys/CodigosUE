22b5690778681f16b572b97b5153fee03c7f4c54
Develop and System <developandsystem@gmail.com>
2023-11-10 10:04:25 +0100

    update

---
+++ PRO/T3/DudasEstructuras/src/Entrada.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+
+        /*
+        Ordenacion
+        int numero1 = 4;
+        int numero2 = 1;
+        int numero3 = 9;
+
+        // numero1, numero2, numero3
+        // numero1, numero3, numero2
+        // numero2, numero1, numero3
+        // numero2, numero3, numero1
+        // numero3, numero1, numero2
+        // numero3, numero2, numero1
+        if(numero1>numero2 && numero1>numero3){
+            if(numero2>numero3){
+                System.out.println(numero1+" "+numero2+" "+numero3);
+            } else {
+                System.out.println(numero1+" "+numero3+" "+numero2);
+            }
+        } else if (){
+        } else if (){
+        }*/
+        /*
+        Ajedrez
+        for (int i = 0; i < 8; i++) {
+            // i=1
+            for (int j = 0; j < 8; j++) {
+                if (i%2==0){
+                    // 1%2 resultado =0 resto=1
+                    if (j%2==0){
+                        System.out.print("   ");
+                    } else {
+                        System.out.print(" # ");
+                    }
+                } else {
+                    if (j%2==0){
+                        System.out.print(" # ");
+                    } else {
+                        System.out.print("   ");
+                    }
+                }
+            }
+            System.out.println();
+        }*/
+        /*
+        // Cuadrado
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i==0 || i==5){
+                    System.out.print("*\t");
+                } else if (j==0 || j==5){
+                    System.out.print("*\t");
+                } else{
+                    System.out.print("-\t");
+                }
+            }
+            System.out.println();
+        }*/
+        /*
+        // Vocal o consonante
+        char letra = 'A';
+        // vocal o consonante
+        switch (letra) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Vocal");
+                break;
+            default:
+                System.out.println("Es consonante");
+        }*/
+        /*
+        //Capicua
+        int numero = 2321;
+        int millares = numero/1000;
+        int restante = numero%1000; // 321
+        int centenas = restante/100;
+        restante = restante%100;// 21
+        int decenas = restante/10;
+        int unidades = restante%10;
+
+        if (unidades == millares) {
+            if (decenas==centenas){
+                System.out.println("Capicua");
+            } else {
+                System.out.println("No capicua");
+            }
+        } else {
+            System.out.println("No capicua");
+        }*/
+
+
+    }
+}
+
