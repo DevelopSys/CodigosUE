629fb32eb44766e09cf8948365ef1125fde4e183
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2024-01-19 12:57:11 +0100

    Update Entrada.java

--- LM/DudasGIT/Dudas/src/Entrada.java
+++ LM/DudasGIT/Dudas/src/Entrada.java
@@ -3,5 +3,6 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.println("Esto es un ejemplo de subida en git");
         // esto es un comenario para poder subirlo a git
+        // cambio realizado desde github
     }
 }
