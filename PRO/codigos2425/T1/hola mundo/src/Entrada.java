30c4318e14f538ba32aa42ff5daad5c9126220b2
Develop and System <developandsystem@gmail.com>
2024-10-02 20:05:35 +0200

    update

---
+++ PRO/codigos2425/T1/hola mundo/src/Entrada.java
@@ -0,0 +1,11 @@
+// .java
+public class Entrada {
+
+    public static void main(String[] args) {
+        System.out.println("Este es mi primer programa en JAVA");
+        System.out.println("Esta es la segunda linea");
+    }
+
+}
+
+
