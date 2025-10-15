7f2fdee900c361fc624a819d9502ecf4f1379f15
Develop and System <developandsystem@gmail.com>
2025-10-15 20:06:32 +0200

    update

---
+++ PRO/codigos2526/T1/HolaMundo/src/Main.java
@@ -0,0 +1,8 @@
+void main(){
+    System.out.println("Ejecucion desde el 25");
+    otroMetodo();
+}
+
+void otroMetodo(){
+    System.out.println("Este es otro metodo para ejecuar en el level 25");
+}
